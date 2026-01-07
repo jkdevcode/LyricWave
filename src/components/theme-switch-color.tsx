717a06f9495e63b8f67880601d7d76734c8f43d2
jkdevcode <dajozavargas@gmail.com>
2026-01-06 20:28:09 -0500

    ✅ feat: enhance navbar with menu items and add tooltips for theme switches

--- src/components/theme-switch-color.tsx
+++ src/components/theme-switch-color.tsx
@@ -5,6 +5,7 @@ import {
   DropdownItem,
 } from "@heroui/dropdown";
 import { Button } from "@heroui/button";
+import { Tooltip } from "@heroui/tooltip";
 
 import { useColorTheme } from "@/hooks/use-color-theme";
 import { HerouiColor } from "@/theme/theme.config";
@@ -23,27 +24,35 @@ export const ThemeColorSwitch = () => {
   ];
 
   return (
-    <Dropdown>
-      <DropdownTrigger>
-        <Button isIconOnly aria-label="Cambiar color del tema" variant="light">
-          <PaletteIcon className={`text-${appColor}`} size={24} />
-        </Button>
-      </DropdownTrigger>
-      <DropdownMenu
-        aria-label="Seleccionar color de tema"
-        selectedKeys={new Set([appColor])}
-        selectionMode="single"
-        onAction={(key) => setAppColor(key as HerouiColor)}
-      >
-        {colors.map((color) => (
-          <DropdownItem key={color} className="capitalize">
-            <div className="flex items-center gap-2">
-              <div className={`w-4 h-4 rounded-full bg-${color}`} />
-              {color}
-            </div>
-          </DropdownItem>
-        ))}
-      </DropdownMenu>
-    </Dropdown>
+    <Tooltip content="Colors Mode" delay={750}>
+      <div>
+        <Dropdown>
+          <DropdownTrigger>
+            <Button
+              isIconOnly
+              aria-label="Cambiar color del tema"
+              variant="light"
+            >
+              <PaletteIcon className={`text-${appColor}`} size={24} />
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu
+            aria-label="Seleccionar color de tema"
+            selectedKeys={new Set([appColor])}
+            selectionMode="single"
+            onAction={(key) => setAppColor(key as HerouiColor)}
+          >
+            {colors.map((color) => (
+              <DropdownItem key={color} className="capitalize">
+                <div className="flex items-center gap-2">
+                  <div className={`w-4 h-4 rounded-full bg-${color}`} />
+                  {color}
+                </div>
+              </DropdownItem>
+            ))}
+          </DropdownMenu>
+        </Dropdown>
+      </div>
+    </Tooltip>
   );
 };
